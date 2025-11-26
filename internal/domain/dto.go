15a39e1cf4eb5251df01d69bfd2f8544d3923358
grepoz <grzegorz.pozorski@gmail.com>
2025-11-26 19:55:48 +0100

    fix

--- internal/domain/dto.go
+++ internal/domain/dto.go
@@ -1,6 +1,10 @@
 package domain
 
-import "github.com/go-playground/validator/v10"
+import (
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
 
 var Validate = validator.New()
 
@@ -28,4 +32,24 @@ type EventDTO struct {
 	EndTime   string  `json:"end_time" validate:"omitempty,datetime=2006-01-02T15:04:05Z07:00"`
 	// Add other fields as needed, with appropriate validation tags
 	// OrganizerName, Country, etc.
+}
+
+func EventDTOToModel(dto *EventDTO) *Event {
+	// Parse time strings to time.Time
+	// Note: We ignore errors here because the validator should have ensured format is correct
+	startTime, _ := time.Parse(time.RFC3339, dto.StartTime)
+	var endTime time.Time
+	if dto.EndTime != "" {
+		endTime, _ = time.Parse(time.RFC3339, dto.EndTime)
+	}
+
+	return &Event{
+		EventName: dto.EventName,
+		City:      dto.City,
+		Type:      dto.Type,
+		Price:     dto.Price,
+		StartTime: startTime,
+		EndTime:   endTime,
+		// Map other fields if necessary
+	}
 }
